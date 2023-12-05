9e6545397c0df3b711dc75a0aa2e8f75fd61e78a
Andreas Marneris <andreas.marneris@dazn.com>
2023-12-05 00:43:14 +0000

    fix: added a function to return string representation of seconds rather than milliseconds for series queries.
    this is to overcome a prometheus proxy issue that only allows seconds

--- pkg/client/api.go
+++ pkg/client/api.go
@@ -24,6 +24,7 @@ import (
 	"net/http"
 	"net/url"
 	"path"
+	"strconv"
 	"strings"
 	"time"
 
@@ -161,10 +162,19 @@ func NewClient(client *http.Client, baseURL *url.URL, headers http.Header, verb 
 	return NewClientForAPI(genericClient, verb)
 }
 
+// String returns an seconds string representation of the Time rather than milliseconds.
+func intString(t model.Time) string {
+	minimumTick := time.Millisecond
+	// second is the Time duration equivalent to one second.
+	second := int64(time.Second / minimumTick)
+	return strconv.FormatInt(int64(t)/int64(second), 10)
+}
+
 func (h *queryClient) Series(ctx context.Context, interval model.Interval, selectors ...Selector) ([]Series, error) {
 	vals := url.Values{}
 	if interval.Start != 0 {
-		vals.Set("start", interval.Start.String())
+		// vals.Set("start", interval.Start.String())
+		vals.Set("start", intString(interval.Start))
 	}
 	if interval.End != 0 {
 		vals.Set("end", interval.End.String())
